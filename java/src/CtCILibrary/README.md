2a5544128a10ddba82904673c45264c07047963e
Sean Poynter <sean.poynter@gmail.com>
2018-07-03 01:27:53 -0500

    Add CtCILibrary package placeholder.

---
+++ java/src/CtCILibrary/README.md
@@ -0,0 +1,2 @@
+Download the files from the book's official GitHub 
+[repository](https://github.com/careercup/CtCI-6th-Edition/tree/master/Java/CtCILibrary/CtCILibrary) to this directory.
---
+++ java/src/CtCILibrary/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author Gayle Laakmann McDowell
+ * The code is probably copyrighted, but this is not mentioned on the GitHub page:
+ * https://github.com/careercup/CtCI-6th-Edition
+ * 
+ * Since there is no license, download the files from the repo and place them here.
+ * https://github.com/careercup/CtCI-6th-Edition/tree/master/Java/CtCILibrary/CtCILibrary
+ */
+package CtCILibrary;
