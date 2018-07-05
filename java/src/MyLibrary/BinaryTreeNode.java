b8a6f9115688dbaa1bc0dbbf063ff70acd55f652
Sean Poynter <sean.poynter@gmail.com>
2018-07-05 02:48:21 -0500

    Add BinaryTreeNode class to MyLibrary package.

---
+++ java/src/MyLibrary/BinaryTreeNode.java
@@ -0,0 +1,13 @@
+package MyLibrary;
+
+public class BinaryTreeNode
+{
+    public int data;
+    public BinaryTreeNode left;
+    public BinaryTreeNode right;
+
+    public BinaryTreeNode(int data)
+    {
+        this.data = data;
+    }
+}
