3ec7aa0567dd6c4c6ea7684dcb88b1572eb4d0da
Sean Poynter <sean.poynter@gmail.com>
2018-07-10 03:00:12 -0500

    Add parent attribute and helper methods for setting left/right children.

--- java/src/MyLibrary/BinaryTreeNode.java
+++ java/src/MyLibrary/BinaryTreeNode.java
@@ -5,9 +5,22 @@ public class BinaryTreeNode
     public int data;
     public BinaryTreeNode left;
     public BinaryTreeNode right;
+    public BinaryTreeNode parent;
 
     public BinaryTreeNode(int data)
     {
         this.data = data;
+    }
+
+    public void setRightChild(BinaryTreeNode rightNode)
+    {
+        this.right = rightNode;
+        rightNode.parent = this;
+    }
+
+    public void setLeftChild(BinaryTreeNode leftChild)
+    {
+        this.left = leftChild;
+        leftChild.parent = this;
     }
 }
