f2e1523ee8c22a46e259548d31d35b31d94ad09b
Sean Poynter <sean.poynter@gmail.com>
2018-10-25 20:25:34 -0500

    Add Java solution for problem 8.7.

--- java/src/ctci/ch8.java
+++ java/src/ctci/ch8.java
@@ -1,5 +1,7 @@
 package ctci;
 
+import java.util.LinkedList;
+
 public class ch8
 {
     // #1
@@ -20,5 +22,31 @@ public class ch8
             return 1;
         }
         return TripleStep(numSteps - 1) + TripleStep(numSteps - 2) + TripleStep(numSteps - 3);
+    }
+
+    // #7
+    public static LinkedList<String> PermutationsWithoutDups(String inpuStr)
+    {
+        /*
+         * Time complexity: O(n!) with respect to the length of the input string
+         * Space complexity: O(n) stack space, O(n!) linked list elements
+         */
+        LinkedList<String> results = new LinkedList<String>();
+        PermutationsWithoutDups("", inpuStr, results);
+        return results;
+    }
+
+    private static void PermutationsWithoutDups(String prefix, String suffix, LinkedList<String> results)
+    {
+        if (suffix.length() == 0)
+        {
+            results.add(prefix);
+        }
+        for (int charPos = 0; charPos < suffix.length(); charPos++)
+        {
+            PermutationsWithoutDups(prefix + suffix.charAt(charPos), 
+                    suffix.substring(0,  charPos) + suffix.substring(charPos + 1, suffix.length()), 
+                    results);
+        }
     }
 }
--- java/src/ctci/ch8Test.java
+++ java/src/ctci/ch8Test.java
@@ -1,5 +1,7 @@
 package ctci;
 
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -9,5 +11,19 @@ public class ch8Test
     void testTripleStepTen()
     {
         assertEquals(4, ch8.TripleStep(3));
+    }
+
+    @Test
+    void testPermutationWithoutDups()
+    {
+        LinkedList<String> results = ch8.PermutationsWithoutDups("abc");
+        LinkedList<String> expected = new LinkedList<String>();
+        expected.add("abc");
+        expected.add("acb");
+        expected.add("bac");
+        expected.add("bca");
+        expected.add("cab");
+        expected.add("cba");
+        assertEquals(expected, results);
     }
 }
