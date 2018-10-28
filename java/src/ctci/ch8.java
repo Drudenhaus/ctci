2962c343b3826a586ce5baa41fab6cd081d1d777
Sean Poynter <sean.poynter@gmail.com>
2018-10-28 02:30:07 -0500

    Add Java solution to problem 8.4.

--- java/src/ctci/ch8.java
+++ java/src/ctci/ch8.java
@@ -1,5 +1,6 @@
 package ctci;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class ch8
@@ -22,6 +23,43 @@ public class ch8
             return 1;
         }
         return TripleStep(numSteps - 1) + TripleStep(numSteps - 2) + TripleStep(numSteps - 3);
+    }
+
+    //# 4
+    public static LinkedList<ArrayList<Integer>> PowerSet(ArrayList<Integer> inputSet, int position)
+    {
+        /*
+         * Set is a misnomer here. I'm using lists to represent the idea of a set in non-comp sci terms.
+         * 
+         * Time complexity: O(n * 2^n) with respect to the number of elements in the "set"
+         *                  n * -> size of a set can be up to the size of the input set
+         *                  2^n -> each element of the input set can be selected or not select
+         * Space complexity: O(n * 2^n)
+         */
+        LinkedList<ArrayList<Integer>> allSets = new LinkedList<ArrayList<Integer>>();
+        if (inputSet.size() == position)
+        {
+            // Base case -- start with empty set then build up from there
+            allSets.add(new ArrayList<Integer>());           
+        }
+        else
+        {
+            // By adding the recursive call first, we'll go through the elements until none remain, add
+            // the empty set, and build up in reverse order of the set
+            allSets = PowerSet(inputSet, position + 1);
+            int currentItem = inputSet.get(position);
+            LinkedList<ArrayList<Integer>> smallerSubSets = new LinkedList<ArrayList<Integer>>();
+            for (ArrayList<Integer> currentset : allSets)
+            {
+                ArrayList<Integer> builtSet = new ArrayList<Integer>();
+                builtSet.add(currentItem);
+                builtSet.addAll(currentset);
+                smallerSubSets.add(builtSet);
+            }
+            allSets.addAll(smallerSubSets);
+        }
+        
+        return allSets;
     }
 
     // #7
--- java/src/ctci/ch8Test.java
+++ java/src/ctci/ch8Test.java
@@ -1,5 +1,7 @@
 package ctci;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,12 +9,31 @@ import org.junit.jupiter.api.Test;
 
 public class ch8Test
 {
+    // #1
     @Test
     void testTripleStepTen()
     {
         assertEquals(4, ch8.TripleStep(3));
     }
 
+    // #4
+    @Test
+    void testPowerSet()
+    {
+        ArrayList<Integer> inputSet = new ArrayList<Integer>(Arrays.asList(1,2,3));
+        LinkedList<ArrayList<Integer>> expectantSets = new LinkedList<ArrayList<Integer>>();
+        expectantSets.add(new ArrayList<Integer>());
+        expectantSets.add(new ArrayList<Integer>(Arrays.asList(3)));
+        expectantSets.add(new ArrayList<Integer>(Arrays.asList(2)));
+        expectantSets.add(new ArrayList<Integer>(Arrays.asList(2, 3)));
+        expectantSets.add(new ArrayList<Integer>(Arrays.asList(1)));
+        expectantSets.add(new ArrayList<Integer>(Arrays.asList(1, 3)));
+        expectantSets.add(new ArrayList<Integer>(Arrays.asList(1, 2)));
+        expectantSets.add(new ArrayList<Integer>(Arrays.asList(1, 2, 3)));
+        assertEquals(expectantSets, ch8.PowerSet(inputSet, 0));
+    }
+
+    // #8
     @Test
     void testPermutationWithoutDups()
     {
